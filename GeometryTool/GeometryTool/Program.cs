97eaf55b9c8b50d1d17323af74828a60fade2410
pmd0064 <peterdouglas@my.unt.edu>
2018-10-14 15:42:16 -0500

    Add files via upload

---
+++ GeometryTool/GeometryTool/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GeometryTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Square square = new Square() { Width = 2 };
+            Triangle triangle = new Triangle() { Base = 2, Height = 5 };
+
+            square.Display();
+            triangle.Display();
+
+        }
+    }
+    abstract class Shape
+    {
+        public abstract int GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("The area is: {0}", GetArea());
+        }
+    }
+    class Square : Shape
+    {
+        public int Width;
+
+        public override int GetArea()
+        {
+            return Width * Width;
+        }
+    }
+    class Triangle : Shape
+    {
+        public int Base;
+        public int Height;
+
+        public override int GetArea()
+        {
+            return Base * Height / 2;
+        }
+    }
+}
